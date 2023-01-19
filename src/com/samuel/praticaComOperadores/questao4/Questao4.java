40d6825b3c426835a9f97fd8dcf4a0c106d7800d
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-18 22:16:21 -0300

    questão 5

--- src/com/samuel/praticaComOperadores/questao4/Questao4.java
+++ src/com/samuel/praticaComOperadores/questao4/Questao4.java
@@ -1,9 +1,25 @@
 package com.samuel.praticaComOperadores.questao4;
 
+import java.util.Scanner;
+
+//4. Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 public class Questao4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double notas = 0;
+		double media;
+		System.out.println("Digite as 4 notas de cada bimestre(aparte enter apos digitar cada): ");
+		notas += scan.nextDouble();
+		notas += scan.nextDouble();
+		notas += scan.nextDouble();
+		notas += scan.nextDouble();
+		
+		media = notas/4;
+		
+		System.out.println("A media do ano e : " + media);
 
 	}
 
