45d6cba2aec0a67b10df8691a97f711d0e1e73cd
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-18 21:43:47 -0300

    Questões 2 a 4
    Praticas com operador de soma, classe Scanner e declaração de variaveis

---
+++ src/com/samuel/praticaComOperadores/questao3/Questão3.java
@@ -0,0 +1,25 @@
+package com.samuel.praticaComOperadores.questao3;
+
+import java.util.Scanner;
+
+//3. Faça um Programa que peça dois números e imprima a soma.
+public class Questão3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int valor1;
+		int valor2;
+		
+		System.out.println("Escreva o primeiro numero: ");
+		valor1 = scan.nextInt();
+		
+		System.out.println("Escreva o segundo numero: ");
+		valor2 = scan.nextInt();
+		
+		System.out.println("O valor somado dos valores e: " + (valor1 + valor2));
+
+	}
+
+}
---
+++ src/com/samuel/praticaComOperadores/questao4/Questao4.java
@@ -0,0 +1,10 @@
+package com.samuel.praticaComOperadores.questao4;
+
+public class Questao4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
